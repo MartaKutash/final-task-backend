a57593659ec9bdf47c1ba74d2f9ddabb591ab27e
Marta <nameless_mk@tut.by>
2023-04-03 13:06:10 +0300

    init: first commit

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://namelessmk:sH7P8nex82pz9H1E@cluster0.t5qom90.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
